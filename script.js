64be1ca9ed653d74417837d90a59e783bdc01c90
kirikrush <perdens@gmail.com>
2018-04-27 21:16:56 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,39 @@
 
 function showMenu() {
     let menu = document.getElementById('nav');
-    if (menu.className == "header__nav") {
-        menu.className = "header__responsive__nav";
+    switch (menu.className) {
+        case "header__nav":
+            menu.className = "header__responsive__nav"
+            break;
+        case "header__responsive__nav":
+            menu.className = "header__nav";
+            break;
     }
-    else{
-        menu.className = "header__nav"
+}
+
+function showBotMenu() {
+    let menu = document.getElementById('foot_nav');
+    switch (menu.className) {
+        case "footer__nav":
+            menu.className = "footer__responsive__nav";
+            break;
+        case "footer__responsive__nav":
+            menu.className = "footer__nav";
+            break;
+    }
+}
+
+
+function hideMenu() {
+    let menu = document.getElementById('nav');
+    if (menu.className = "header__responsive__nav") {
+        menu.className = "header__nav";
+    }
+}
+
+function hideBotMenu() {
+    let menu = document.getElementById('foot_nav');
+    if (menu.className = "footer__responsive__nav") {
+        menu.className = "footer__nav";
     }
 }
