7c52844a368242c7ce8b553b6b6191d4cce58152
kirikrush <perdens@gmail.com>
2018-05-07 23:29:56 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
+
 $(document).ready(() => {
+  let current_ask = '#question__one';
+
   $('.response__menu__icon').on('click', () => {
     switch ($('#nav').attr('class')) {
       case 'header__nav':
@@ -30,34 +33,123 @@ $(document).ready(() => {
     }
   })
 
-  $(".top__desc__test").on('click', () =>{
+  $(".top__desc__test").on('click', () => {
     $(".container_test").css({
-      transform:"scale(1)"
+      transform: "scale(1)"
     })
     $(".test_div").css({
-      transform:"scale(1)"
+      transform: "scale(1)"
     })
     $("body").css({
-      overflow:"hidden"
+      overflow: "hidden"
     })
 
 
   })
 
-  $('.test__close').on('click', () =>{
+  $('.test__close').on('click', () => {
     $(".container_test").css({
-      transform:"scale(0)"
+      transform: "scale(0)"
     })
     $(".test_div").css({
-      transform:"scale(0)"
+      transform: "scale(0)"
     })
 
     $("body").css({
-      overflow:"scroll"
+      overflow: "scroll"
     })
 
-})
+  })
+  $('#next').on('click', () => {
+    switch (current_ask) {
+      case '#question__one':
+        $(current_ask).css({
+          "height":"0",
+          "width":"0",
+          "opacity":"0",
+          "right":"-300px"
+        });
+        current_ask = '#question__two';
+        $(current_ask).css({
+          "height":"initial",
+          "right": "0",
+          "width":"initial",
+          "opacity":"1"
+        });
+        $('#test__progress').css("width", "20%");
+        $('#test__progress').removeClass().addClass("progress2");
+        break;
+      case '#question__two':
+        $(current_ask).css({
+          "height":"0",
+          "width":"0",
+          "opacity":"0",
+          "right":"-300px"
+        });
+        current_ask = '#question__three';
+        $(current_ask).css({
+          "height":"initial",
+          "right": "0",
+          "width":"initial",
+          "opacity":"1"
+        });
+        $('#test__progress').css("width", "40%");
+        $('#test__progress').removeClass("progress2").addClass("progress3")
+        break;
+      case '#question__three':
+        $(current_ask).css({
+          "height":"0",
+          "width":"0",
+          "opacity":"0",
+          "right":"-300px"
+        });
+        current_ask = '#question__four';
+        $(current_ask).css({
+          "height":"initial",
+          "right": "0",
+          "width":"initial",
+          "opacity":"1"
+        });
+        $('#test__progress').css("width", "60%");
+        $('#test__progress').removeClass("progress3").addClass("progress4")
+        break;
+      case '#question__four':
+        $(current_ask).css({
+          "height":"0",
+          "width":"0",
+          "opacity":"0",
+          "right":"-300px"
+        });
+        current_ask = '#question__five';
+        $(current_ask).css({
+          "height":"initial",
+          "right": "0",
+          "width":"initial",
+          "opacity":"1"
+        });
+        $('#test__progress').css("width", "80%");
+        $('#test__progress').removeClass("progress4").addClass("progress5")
+        break;
+      case '#question__five':
+        $(current_ask).css({
+          "height":"0",
+          "width":"0",
+          "opacity":"0",
+          "right":"-300px"
+        });
+        current_ask = '#question__one';
+        $(current_ask).css({
+          "height":"initial",
+          "right": "0",
+          "width":"initial",
+          "opacity":"1"
+        });
+        $('#test__progress').css("width", "100%");
+        $('#test__progress').removeClass("progress5").addClass("progress6")
+        break;
 
+    }
+  })
 })
 
 
