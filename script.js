2c2e654c5c27a44848aee5b369af053a63e680b6
kirikrush <perdens@gmail.com>
2018-05-22 10:48:38 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -6,36 +6,10 @@ $(document).ready(() => {
   }
   let current_label="#null";
 
-  $('.response__menu__icon').on('click', () => {
-    switch ($('#nav').attr('class')) {
-      case 'header__nav':
-        $('#nav').attr('class', "header__responsive__nav");
-        break;
-      case "header__responsive__nav":
-        $('#nav').attr('class', "header__nav");
-        break;
-    }
-
+  $('.relat').on('click', () => {
+    $(".hidden__menu").toggle();
   })
 
-
-
-  $('#response__menu__icon').on('click', () => {
-    switch ($('#foot_nav').attr('class')) {
-      case 'footer__nav':
-        $('#foot_nav').attr('class', "footer__responsive__nav");
-        break;
-      case "footer__responsive__nav":
-        $('#foot_nav').attr('class', "footer__nav");
-        break;
-    }
-  })
-
-  $("section").on('click', () => {
-    if ($('#foot_nav').attr('class') == "footer__responsive__nav") {
-      $('#foot_nav').attr('class', "footer__nav");
-    }
-  })
 
   $(".top__desc__test").on('click', () => {
     $(".container_test").css({
